916b8c477caed9bd0411e403c2c2113cf387875a
Jhon-Mota <jhonathan.nasc21@gmail.com>
2025-01-16 12:20:13 -0300

    Class 'RestExceptionHandler' created.

---
+++ src/main/java/project/financialplanning/handling/RestExceptionHandler.java
@@ -0,0 +1,9 @@
+package project.financialplanning.handling;
+
+import org.springframework.http.ResponseEntity;
+
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
+
+    
+    
+}
